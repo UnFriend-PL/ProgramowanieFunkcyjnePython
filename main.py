a1e161844c682f9f2eccd73f40ce7f1bbb1a6d01
UnFriend-PL <szymonecki1233@gmail.com>
2024-02-20 16:04:27 +0100

    zad_1

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+def is_even(x):
+    return x % 2 == 0
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+even_numbers = list(filter(is_even, numbers))
+print(even_numbers)
 
+square = lambda x: x ** 2
+print(square(5))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+names = ["Anna", "Bob", "Charlie"]
+ages = [25, 30, 22]
+combined = list(zip(names, ages))
+print(combined)
 
+fruits = ["apple", "banana", "orange"]
+for index, fruit in enumerate(fruits):
+    print(f"Index: {index}, Fruit: {fruit}")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+characters = ['a', 'A', 'z', 'Z', '1', '9', '@', '$']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+min_char = min(characters, key=lambda x: ord(x))
+max_char = max(characters, key=lambda x: ord(x))
+
+print("Najmniejszy znak ASCII:", min_char)
+print("Największy znak ASCII:", max_char)
